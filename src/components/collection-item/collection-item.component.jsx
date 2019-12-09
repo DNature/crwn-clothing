6030284da1596574cdfe0d7d3901306db46545f6
osoka <victorosoka0@gmail.com>
2019-12-09 11:25:37 +0100

    styled collection item component to have hover effect

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import CustomButton from '../custom-button/custom-button.component'
+
 import "./collection-item.styles.scss";
+
 
 const CollectionItem = ({ id, name, price, imageUrl }) => {
   return (
@@ -9,6 +12,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => {
         <span className="name">{name}</span>
         <span className="price">{"$" + price}</span>
       </div>
+      <CustomButton inverted >ADD TO CART</CustomButton>
     </div>
   );
 };
