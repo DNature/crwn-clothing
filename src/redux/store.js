b6c36907d393bf5f63fea458dd53c2fe513a37cd
osoka <victorosoka0@gmail.com>
2019-12-09 12:01:59 +0100

    Updated shop data with new data, updated cart utils with group item shop reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(
+  rootReducer,
+  // applyMiddleware(...middlewares)
+  composeWithDevTools()
+);
 
 export default store;
