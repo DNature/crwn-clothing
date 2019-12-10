8b196db2820b10222fa67cfb5cdfd18d12a3e5f2
Divine Nature <divinehycenth@outlook.com>
2019-12-10 17:54:35 +0100

    Removed redux-logger and removed redux store in prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
-import logger from 'redux-logger';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger];
-
 export const store = createStore(
   rootReducer,
-  // applyMiddleware(...middlewares)
-  composeWithDevTools()
+  process.env.NODE_ENV === 'development' && composeWithDevTools()
 );
 
 export const persistor = persistStore(store);
