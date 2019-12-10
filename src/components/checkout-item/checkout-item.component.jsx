6f4c557113c38e7c261bfbecccf47fa4803bdfcd
osoka <victorosoka0@gmail.com>
2019-12-10 05:31:41 +0100

    Persisted cart items to localstorage

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss'
+import { addItem, removeItem } from '../../redux/cart/cart.actions';
 
-const CheckoutItem = ({ cartItem: { imageUrl, name, price, quantity } }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
+
+  const { imageUrl, name, price, quantity } = cartItem
 
   return (
     <div className="checkout-item">
@@ -10,12 +16,24 @@ const CheckoutItem = ({ cartItem: { imageUrl, name, price, quantity } }) => {
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name">{name}</span>
-      <span className="quantity">{quantity}</span>
+      <span className="quantity">
+        <div className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow" onClick={() => addItem(cartItem)}>&#10095;</div>
+      </span>
       <span className="price">${price}</span>
-      <div className="remove-button">&#9747;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        <div className="icon">&#9747;</div>
+        <p className="icon-text">Remove</p>
+      </div>
     </div>
   )
 }
 
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item))
+})
 
-export default CheckoutItem
+export default connect(null, mapDispatchToProps)(CheckoutItem)
